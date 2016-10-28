163d4a08f9e6b1883556a3ff1760b183ad896364
GGG KILLER <gggkiller2@gmail.com>
2016-10-28 20:38:26 -0200

    Added serialization and type conversion checking utilities

---
+++ Kemori/Utils/Is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemori.Utils
+{
+    static class Is
+    {
+        public static Boolean Int ( String s )
+        {
+            Int32 result;
+            return Int32.TryParse ( s, out result );
+        }
+    }
+}
---
+++ Kemori/Utils/SerializerUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemori.Utils
+{
+    class SerializerUtils
+    {
+        public static void SerializeToFile ( Object graph, String FileName )
+        {
+            var formatter = new BinaryFormatter
+            {
+                AssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Full,
+                TypeFormat = System.Runtime.Serialization.Formatters.FormatterTypeStyle.XsdString
+            };
+
+            using ( var stream = File.Open ( FileName, FileMode.OpenOrCreate ) )
+                formatter.Serialize ( stream, graph );
+        }
+
+        public static T DeserializeFromFile<T>(String FileName)
+        {
+            var formatter = new BinaryFormatter
+            {
+                AssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Full,
+                TypeFormat = System.Runtime.Serialization.Formatters.FormatterTypeStyle.XsdString
+            };
+
+            using ( var stream = File.Open ( FileName, FileMode.Open ) )
+                return ( T ) formatter.Deserialize ( stream );
+        }
+    }
+}

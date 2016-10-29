c774d113fa7441a8614b8067c86ed48d7bec8362
GGG KILLER <gggkiller2@gmail.com>
2016-10-29 06:45:50 -0200

    Added DataGridView progress indicator columns from GUtils

---
+++ Kemori/UI/DataGridViewProgressCell.cs
@@ -0,0 +1,77 @@
+﻿namespace GUtils.Forms.DataGridView
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    // Modified from:
+    // https://social.msdn.microsoft.com/Forums/windows/en-US/769ca9d6-1e9d-4d76-8c23-db535b2f19c2/sample-code-datagridview-progress-bar-column?forum=winformsdatacontrols
+
+    internal class DataGridViewProgressCell : DataGridViewImageCell
+    {
+        // Used to make custom cell consistent with a DataGridViewImageCell
+        private static readonly Image emptyImage;
+        // Create the Color only once to avoid having to re-create it all the time (and for each instance)
+        private static readonly Color progressBarColor = Color.FromArgb ( 203, 235, 108 );
+
+        static DataGridViewProgressCell ( )
+        {
+            emptyImage = new Bitmap ( 1, 1, System.Drawing.Imaging.PixelFormat.Format32bppArgb );
+        }
+
+        public DataGridViewProgressCell ( )
+        {
+            this.ValueType = typeof ( Int32 );
+        }
+
+        // Method required to make the Progress Cell consistent with the default Image Cell.
+        // The default Image Cell assumes an Image as a value, although the value of the Progress Cell is an int.
+        protected override Object GetFormattedValue ( Object value, Int32 rowIndex, ref DataGridViewCellStyle cellStyle, TypeConverter valueTypeConverter, TypeConverter formattedValueTypeConverter, DataGridViewDataErrorContexts context )
+        {
+            return emptyImage;
+        }
+
+        protected override void Paint ( Graphics g, Rectangle clipBounds, Rectangle cellBounds, Int32 rowIndex, DataGridViewElementStates cellState, Object value, Object formattedValue, String errorText, DataGridViewCellStyle cellStyle, DataGridViewAdvancedBorderStyle advancedBorderStyle, DataGridViewPaintParts paintParts )
+        {
+            try
+            {
+                var progressVal = ( int ) value;
+                var percentage = ( ( float ) progressVal / 100.0f ); // Need to convert to float before division; otherwise C# returns int which is 0 for anything but 100%.
+                using ( Brush backColorBrush = new SolidBrush ( cellStyle.BackColor ) )
+                using ( Brush foreColorBrush = new SolidBrush ( cellStyle.ForeColor ) )
+                {
+                    // Draws the cell grid
+                    base.Paint ( g, clipBounds, cellBounds,
+                     rowIndex, cellState, value, formattedValue, errorText,
+                     cellStyle, advancedBorderStyle, ( paintParts & ~DataGridViewPaintParts.ContentForeground ) );
+                    if ( percentage > 0.0 )
+                    {
+                        // Draw the progress bar and the text
+                        using ( var solidBrush = new SolidBrush ( Color.FromArgb ( 203, 235, 108 ) ) )
+                        {
+                            // Draw the progress bar and the text
+                            g.FillRectangle ( solidBrush, cellBounds.X + 2, cellBounds.Y + 2, Convert.ToInt32 ( ( percentage * cellBounds.Width - 4 ) ), cellBounds.Height - 4 );
+                        }
+                        g.DrawString ( progressVal.ToString ( ) + "%", cellStyle.Font, foreColorBrush, cellBounds.X + ( cellBounds.Width / 2 ) - 5, cellBounds.Y + 2 );
+
+                    }
+                    else
+                    {
+                        // draw the text
+                        if ( this.DataGridView.CurrentRow.Index == rowIndex )
+                            using ( var solidBrush = new SolidBrush ( cellStyle.SelectionForeColor ) )
+                            {
+                                g.DrawString ( progressVal.ToString ( ) + "%", cellStyle.Font, solidBrush, cellBounds.X + 6, cellBounds.Y + 2 );
+                            }
+                        else
+                            g.DrawString ( progressVal.ToString ( ) + "%", cellStyle.Font, foreColorBrush, cellBounds.X + 6, cellBounds.Y + 2 );
+                    }
+                }
+            }
+#pragma warning disable CC0004 // Catch block cannot be empty
+            catch ( Exception ) { }
+#pragma warning restore CC0004 // Catch block cannot be empty
+        }
+    }
+}
---
+++ Kemori/UI/DataGridViewProgressColumn.cs
@@ -0,0 +1,11 @@
+﻿namespace GUtils.Forms.DataGridView
+{
+    // Source: https://social.msdn.microsoft.com/Forums/windows/en-US/769ca9d6-1e9d-4d76-8c23-db535b2f19c2/sample-code-datagridview-progress-bar-column?forum=winformsdatacontrols
+    public class DataGridViewProgressColumn : System.Windows.Forms.DataGridViewImageColumn
+    {
+        public DataGridViewProgressColumn ( )
+        {
+            CellTemplate = new DataGridViewProgressCell ( );
+        }
+    }
+}
